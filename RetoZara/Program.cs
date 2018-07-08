ef8de5752bd12ed15038f6ab83af28a485e7a53d
HARRISONS-MK\G1 <harrison_silva_2@hotmail.com>
2018-07-09 01:57:16 +0200

    Primer commit con la estructura del proyecto. close #1

---
+++ RetoZara/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace RetoZara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
